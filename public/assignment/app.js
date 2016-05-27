87768f9d36565eb8eb7655d43a5c2a887d11a211
Nikhil Kathuria <nikhil.kathuria@gmail.com>
2016-05-26 23:29:50 -0400

    Changed directory structure. Added Angular app and routes

---
+++ public/assignment/app.js
@@ -0,0 +1,5 @@
+// IFFE
+
+(function(){
+    angular.module("WebAppMaker", ["ngRoute"]);
+})();
---
+++ public/assignment/config.js
@@ -0,0 +1,11 @@
+
+(function(){
+    angular
+        .module("WebAppMaker")
+        .config(Config);
+    
+    function Config($routeProvider){
+        $routeProvider
+    }
+    
+})();
