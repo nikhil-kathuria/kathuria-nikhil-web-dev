8cdad53eb687f1a74d02c4361cc6338bcae549a1
Nikhil Kathuria <nikhil.kathuria@gmail.com>
2016-05-28 12:45:48 -0400

    Added page list functionality

---
+++ public/assignment/services/page.service.client.js
@@ -0,0 +1,73 @@
+(function(){
+    angular
+        .module("WebAppMaker")
+        .factory("PageService", PageService);
+
+    function PageService() {
+        var pages = [
+            { "_id": "321", "name": "Post 1", "websiteId": "456" },
+            { "_id": "432", "name": "Post 2", "websiteId": "456" },
+            { "_id": "543", "name": "Post 3", "websiteId": "456" }
+        ];
+
+        var api = {
+            createPage: createPage,
+            findPageByWebsite: findPageByWebsite,
+            findPageById: findPageById,
+            updatePage: updatePage,
+            deletePage: deletePage
+        };
+
+        return api;
+
+
+        function createPage(websiteId, page) {
+            page.websiteId = websiteId;
+            pages.push(page);
+            return page;
+        }
+
+        function findPageByWebsite(websiteId) {
+            var resultSet = [];
+            for (var idx in  pages) {
+                if (pages[idx].websiteId === websiteId) {
+                    resultSet.push(pages[idx]);
+                }
+            }
+            return resultSet;
+        }
+
+        function findPageById(pageId) {
+            var resultSet = [];
+            for (var idx in  pages) {
+                if (pages[idx]._id === pageId) {
+                    resultSet.push(pages[idx]);
+                }
+            }
+            return resultSet;
+
+        }
+
+        function updatePage(pageId, page) {
+            for (var idx in pages) {
+                if (pages[idx]._id === pageId) {
+                    pages[idx] = page;
+                    return page;
+                }
+            }
+            return null;
+
+        }
+
+        function deletePage(pageId) {
+            for (var idx in pages) {
+                if (pages[idx]._id === pageId) {
+                    pages.splice(idx, 1);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+})();
